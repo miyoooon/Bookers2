cb7e10c591fbf5e74077358fdc3823af6539f530
miyoooon <133766145+miyoooon@users.noreply.github.com>
2023-06-28 13:39:03 +0000

    [Add] Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,17 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
+    @user = current_user
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to
+
   end
 
   def edit
@@ -10,4 +21,10 @@ class BooksController < ApplicationController
 
   def show
   end
+
+  private
+  def book_params
+  params.require(:book).permit(:title,:body)
+  end
+
 end

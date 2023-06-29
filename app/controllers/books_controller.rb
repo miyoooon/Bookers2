f58b824570eafbed3aaba2fd8eafc874d7aece88
miyoooon <133766145+miyoooon@users.noreply.github.com>
2023-06-29 15:55:07 +0000

    [Add] Bookers2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def new
+     @book = Book.new
   end
 
   def index
@@ -12,7 +13,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to
+    redirect_to book_path(@book.id)
 
   end
 
@@ -20,6 +21,17 @@ class BooksController < ApplicationController
   end
 
   def show
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
+    @booker = Book.find(params[:id])
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+    
   end
 
   private
